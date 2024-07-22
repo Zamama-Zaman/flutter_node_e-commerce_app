0d35775142ac93ebd86a51f5529fa94bf9fa1a84
zamama-zaman <zamamazaman@gmail.com>
2024-07-22 07:33:01 +0500

    Error handler added on server side

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 connectDb();
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use("/", require("./routes/userRoutes"));
 
 app.use("/", require("./routes/productRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
