8aa5906114c8a166671d7325044c7639870d35a5
zamama-zaman <zamamazaman@gmail.com>
2024-09-09 23:11:34 +0500

    Unit Testing continoue ...

--- test/core/network/services/home_service/home_service_test.dart
+++ test/core/network/services/home_service/home_service_test.dart
@@ -238,6 +238,75 @@ void main() {
         (l) => expect(l, isA<String>()),
         (r) => expect(r, isA<List<Product>>()),
       );
+
+      test("On empty send response return not found", () async {
+        const search = "";
+
+        Map<String, String> headers = {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept-Language': 'en',
+        };
+
+        /// Arrange
+        when(() => middleWareClient.get(
+              Uri.parse("${AppBaseUrl.getProduct}$search"),
+              headers: headers,
+            )).thenAnswer(
+          (invocation) async => Response(
+            """
+                {
+                  "status": true,
+                  "data": [
+                      {
+                          "_id": "6678d547b75d44bd2ca18de7",
+                          "name": "Laptop",
+                          "price": "20",
+                          "category": "Mobile",
+                          "quantity": "10",
+                          "description": "This is an new laptop",
+                          "images": [
+                              "https://res.cloudinary.com/dm0exb8fh/image/upload/v1719194944/Laptop/rvratodms2hunddeerqg.jpg",
+                              "https://res.cloudinary.com/dm0exb8fh/image/upload/v1719194948/Laptop/pcqewzi313hbxcw0e04t.jpg",
+                              "https://res.cloudinary.com/dm0exb8fh/image/upload/v1719194950/Laptop/tlesg67mahz44nqg1nz1.jpg"
+                          ],
+                          "rating": [
+                              {
+                                  "rate": "3",
+                                  "productId": "6678d547b75d44bd2ca18de7",
+                                  "userId": "6668f8c8c04a89576e2840f5",
+                                  "_id": "668ca3b6d32da622aba2f5f5"
+                              },
+                              {
+                                  "rate": "4.0",
+                                  "productId": "6678d547b75d44bd2ca18de7",
+                                  "userId": "666ba50210b52b5a8193681b",
+                                  "_id": "668f3666f093cb4d3ae98d92"
+                              }
+                          ],
+                          "createdAt": "2024-06-24T02:09:11.370Z",
+                          "updatedAt": "2024-07-11T01:33:26.303Z",
+                          "__v": 2
+                      }
+                  ]
+              }
+            """,
+            200,
+          ),
+        );
+
+        /// Act
+        final result = await homeService.searchProduct(search: search);
+
+        /// Assert
+        expect(result, isA<Either<String, List<Product>>>());
+        expect(result.isRight(), true);
+        result.fold(
+          (l) => expect(l, isA<String>()),
+          (r) => expect(r, isA<List<Product>>()),
+        );
+
+        //
+      });
     });
   });
 }
