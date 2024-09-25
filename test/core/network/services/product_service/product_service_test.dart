3e49e2b5f24193fa1222968e90bcf10a53943240
zamama-zaman <zamamazaman@gmail.com>
2024-09-25 20:10:35 +0500

    Unit test Continoue...

--- test/core/network/services/product_service/product_service_test.dart
+++ test/core/network/services/product_service/product_service_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_node_ecommerce_app_original/common/models/cart_model.dart';
 import 'package:flutter_node_ecommerce_app_original/lib.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -8,7 +9,7 @@ class MockCustomHttpClientMiddleWare extends Mock
 class MockSharePreference extends Mock implements SharedPreferences {}
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  // TestWidgetsFlutterBinding.ensureInitialized();
   late ProductService productService;
   late MockCustomHttpClientMiddleWare middleWareClient;
   late AppPreference appPreference;
@@ -272,6 +273,195 @@ void main() {
       /// Assert
       expect(result, isA<Either<String, String>>());
       expect(result.isLeft(), true);
+    });
+  });
+
+  group("Get Cart -", () {
+    test(
+        "The API call returns a status code of 200, and the cart is successfully retrieved and parsed.",
+        () async {
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.get(
+          Uri.parse(AppBaseUrl.getCartUrl),
+          headers: headers,
+        ),
+      ).thenAnswer(
+        (invocation) async => Response(
+          """
+            {
+              "status": "Success",
+              "message": "Successfully fetch carts",
+              "body": {
+                  "cart": [
+                      {
+                          "product": {
+                              "adminId": "666ba6a610b52b5a8193681e",
+                              "name": "Laptop",
+                              "price": "20",
+                              "category": "Mobile",
+                              "quantity": "10",
+                              "description": "This is an new laptop",
+                              "images": [
+                                  "https://res.cloudinary.com/dm0exb8fh/image/upload/v1719194944/Laptop/rvratodms2hunddeerqg.jpg",
+                                  "https://res.cloudinary.com/dm0exb8fh/image/upload/v1719194948/Laptop/pcqewzi313hbxcw0e04t.jpg",
+                                  "https://res.cloudinary.com/dm0exb8fh/image/upload/v1719194950/Laptop/tlesg67mahz44nqg1nz1.jpg"
+                              ],
+                              "rating": [
+                                  {
+                                      "rate": "4.5",
+                                      "productId": "6678d547b75d44bd2ca18de7",
+                                      "userId": "6668f8c8c04a89576e2840f5",
+                                      "_id": "668ca3b6d32da622aba2f5f5"
+                                  },
+                                  {
+                                      "rate": "4.6",
+                                      "productId": "6678d547b75d44bd2ca18de7",
+                                      "userId": "666ba50210b52b5a8193681b",
+                                      "_id": "668f3666f093cb4d3ae98d92"
+                                  }
+                              ],
+                              "_id": "6678d547b75d44bd2ca18de7",
+                              "createdAt": "2024-06-24T02:09:11.370Z",
+                              "updatedAt": "2024-09-24T15:36:29.641Z",
+                              "__v": 2
+                          },
+                          "quantity": 1,
+                          "_id": "66f2dc7d47fc602b62728b62"
+                          }
+                        ]
+                      }
+                    }
+            """,
+          200,
+        ),
+      );
+
+      /// Act
+      final result = await productService.getCart();
+
+      /// Assert
+      expect(result, isA<Either<String, List<CartModel>>>());
+      expect(result.isRight(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<List<CartModel>>()),
+      );
+    });
+
+    test("The API call returns a non-200 status code, indicating failure.",
+        () async {
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.get(
+          Uri.parse(AppBaseUrl.getCartUrl),
+          headers: headers,
+        ),
+      ).thenAnswer(
+        (invocation) async => Response(
+          """
+            {
+              "status": "fails",
+              "message": "Failed to fetch cart"
+            }
+            """,
+          400,
+        ),
+      );
+
+      /// Act
+      final result = await productService.getCart();
+
+      /// Assert
+      expect(result, isA<Either<String, List<CartModel>>>());
+      expect(result.isLeft(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<List<CartModel>>()),
+      );
+      expect(result.fold((l) => l, (r) => ''), 'Failed to fetch cart');
+    });
+
+    test("An exception occurs during the API call, such as a network error.",
+        () async {
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.get(
+          Uri.parse(AppBaseUrl.getCartUrl),
+          headers: headers,
+        ),
+      ).thenThrow(Exception('Network error'));
+
+      /// Act
+      final result = await productService.getCart();
+
+      /// Assert
+      expect(result, isA<Either<String, List<CartModel>>>());
+      expect(result.isLeft(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<List<CartModel>>()),
+      );
+      expect(result.fold((l) => l, (r) => ''), contains('Error Get Cart'));
+    });
+
+    test("The API call is successful (200) but the cart is empty.", () async {
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.get(
+          Uri.parse(AppBaseUrl.getCartUrl),
+          headers: headers,
+        ),
+      ).thenAnswer((a) async {
+        return Response(
+          """
+          {
+              "status": "Success",
+              "message": "Successfully fetch carts",
+              "body": {
+                  "cart": []
+                }
+          }
+          """,
+          200,
+        );
+      });
+
+      /// Act
+      final result = await productService.getCart();
+
+      /// Assert
+      expect(result, isA<Either<String, List<CartModel>>>());
+      expect(result.isRight(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<List<CartModel>>()),
+      );
+      expect(result.getOrElse(() => []).isEmpty, true);
     });
 
     // Group End
