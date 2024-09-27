44baedccc01d9cae9b08c84f5e556bb7ea8a03ed
zamama-zaman <zamamazaman@gmail.com>
2024-09-27 22:59:32 +0500

    Unit Test Continoue...

--- test/core/network/services/product_service/product_service_test.dart
+++ test/core/network/services/product_service/product_service_test.dart
@@ -463,9 +463,271 @@ void main() {
       );
       expect(result.getOrElse(() => []).isEmpty, true);
     });
-
-    // Make Plan for next Unit Test
-
     // Group End
+  });
+
+  group("Remove From Cart -", () {
+    test(
+        "The API call returns a status code of 200, indicating the product was successfully removed from the cart.",
+        () async {
+      Product product = Product.fromMap(const {"_id": "0"});
+      final myJsonEncode = json.encode({
+        "productId": product.id,
+      });
+
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.post(
+          Uri.parse(AppBaseUrl.removeFromCartUrl),
+          body: myJsonEncode,
+          headers: headers,
+        ),
+      ).thenAnswer(
+        (invocation) async => Response(
+          """
+                {
+                    "status": true,
+                    "message": "Product removed from cart successfully"
+                }
+            """,
+          200,
+        ),
+      );
+
+      /// Act
+      final result = await productService.removeFromCart(product: product);
+
+      /// Assert
+      expect(result, isA<Either<String, String>>());
+      expect(result.isRight(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<String>()),
+      );
+      expect(
+          result.getOrElse(() => ''), 'Product removed from cart successfully');
+    });
+
+    test("The API call returns a non-200 status code, indicating failure.",
+        () async {
+      Product product = Product.fromMap(const {"_id": "0"});
+      final myJsonEncode = json.encode({
+        "productId": product.id,
+      });
+
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.post(
+          Uri.parse(AppBaseUrl.removeFromCartUrl),
+          body: myJsonEncode,
+          headers: headers,
+        ),
+      ).thenAnswer(
+        (invocation) async => Response(
+          """
+                {
+                    "status": true,
+                    "message": "Failed to remove product from cart"
+                }
+            """,
+          400,
+        ),
+      );
+
+      /// Act
+      final result = await productService.removeFromCart(product: product);
+
+      /// Assert
+      expect(result, isA<Either<String, String>>());
+      expect(result.isLeft(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<String>()),
+      );
+      expect(result.fold((l) => l, (r) => ''),
+          'Failed to remove product from cart');
+    });
+
+    test("An exception occurs during the API call, such as a network error.",
+        () async {
+      Product product = Product.fromMap(const {"_id": "0"});
+      final myJsonEncode = json.encode({
+        "productId": product.id,
+      });
+
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.post(
+          Uri.parse(AppBaseUrl.removeFromCartUrl),
+          body: myJsonEncode,
+          headers: headers,
+        ),
+      ).thenThrow(
+        Exception('Network error'),
+      );
+
+      /// Act
+      final result = await productService.removeFromCart(product: product);
+
+      /// Assert
+      expect(result, isA<Either<String, String>>());
+      expect(result.isLeft(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<String>()),
+      );
+      expect(
+          result.fold((l) => l, (r) => ''), contains('Error Remove From Cart'));
+    });
+
+    test("The product object has an invalid or empty id.", () async {
+      Product product = Product.fromMap(const {"_id": ""});
+      final myJsonEncode = json.encode({
+        "productId": product.id,
+      });
+
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(() => middleWareClient.post(
+            Uri.parse(AppBaseUrl.removeFromCartUrl),
+            body: myJsonEncode,
+            headers: headers,
+          )).thenAnswer(
+        (invocation) async => Response(
+          """
+                {
+                    "status": true,
+                    "message": "Invalid product ID"
+                }
+            """,
+          400,
+        ),
+      );
+
+      /// Act
+      final result = await productService.removeFromCart(product: product);
+
+      /// Assert
+      expect(result, isA<Either<String, String>>());
+      expect(result.isLeft(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<String>()),
+      );
+      expect(result.fold((l) => l, (r) => ''), 'Invalid product ID');
+    });
+
+    test("Verifies that the correct headers are used for removeFromCart",
+        () async {
+      Product product = Product.fromMap(const {"_id": ""});
+      final myJsonEncode = json.encode({
+        "productId": product.id,
+      });
+
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      var capturedHeaders;
+
+      /// Arrange
+      when(() => middleWareClient.post(
+            Uri.parse(AppBaseUrl.removeFromCartUrl),
+            body: myJsonEncode,
+            headers: headers,
+          )).thenAnswer(
+        (invocation) async {
+          capturedHeaders = invocation.namedArguments[#headers];
+          return Response(
+            '{"message": "Product removed from cart successfully"}',
+            200,
+          );
+        },
+      );
+
+      /// Act
+      final result = await productService.removeFromCart(product: product);
+
+      /// Assert
+      expect(result, isA<Either<String, String>>());
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<String>()),
+      );
+      // Verify headers contain the necessary fields
+      expect(capturedHeaders['Authorization'], contains('Bearer'));
+      expect(capturedHeaders['Accept-Language'], isNotNull);
+    });
+
+    test(
+        "The API response is a 200 status but contains malformed or unexpected JSON.",
+        () async {
+      Product product = Product.fromMap(const {"_id": ""});
+      final myJsonEncode = json.encode({
+        "productId": product.id,
+      });
+
+      Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept-Language': 'en',
+        'Authorization': 'Bearer ${appPreference.getUserModel.token}',
+      };
+
+      /// Arrange
+      when(
+        () => middleWareClient.post(
+          Uri.parse(AppBaseUrl.removeFromCartUrl),
+          body: myJsonEncode,
+          headers: headers,
+        ),
+      ).thenAnswer(
+        (invocation) async => Response(
+          """
+                {
+            """,
+          200,
+        ),
+      );
+
+      /// Act
+      final result = await productService.removeFromCart(product: product);
+
+      /// Assert
+      expect(result, isA<Either<String, String>>());
+      expect(result.isLeft(), true);
+      result.fold(
+        (l) => expect(l, isA<String>()),
+        (r) => expect(r, isA<String>()),
+      );
+      // expect(
+      //     result.getOrElse(() => ''), 'Product removed from cart successfully');
+    });
+
+    //Group End
   });
 }
