4fb6c0859a519950dc1158da0191525a63e893df
zamama-zaman <zamamazaman@gmail.com>
2024-11-19 04:42:17 +0500

    19-11-24
    - Plan to Create UI for chat View.

--- lib/main.dart
+++ lib/main.dart
@@ -54,5 +54,5 @@ class MyApp extends StatelessWidget {
 /// 
 
 
-/// Today's Report 13-11-24:
-/// - Completed watching tutorial for socket.
+/// Today's Report 19-11-24:
+/// - Plan to Create UI for chat View.
